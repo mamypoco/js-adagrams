6b0c8ad340549ea802e6d8c22388d86af24f27b9
mami <mamypoco@gmail.com>
2025-11-20 16:48:41 -0800

    wave01 passed, wip wave02

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,54 @@
+import { LETTER_POOL } from './constants.js';
+
+const HAND_SIZE = 10;
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let trackingPool = [];
+
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i < count; i++) {
+      trackingPool.push(letter);
+    }
+  }
+  console.log(trackingPool);
+
+  let letters = [];
+
+  for (let i = 0; i < HAND_SIZE; i++) {
+    const randonIndex = Math.floor(Math.random() * trackingPool.length);
+    letters.push(trackingPool[randonIndex]);
+    trackingPool[randonIndex] = trackingPool[trackingPool.length - 1];
+    trackingPool.pop();
+  }
+  // console.log(letters)
+  return letters;
+
 };
+// drawLetters()
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  // input = dog, lettersInHand = letters 'daioqgnmac' 
+  // create shallow copy for tracking
+  input = input.toUpperCase();
+  let updatedLettersInHand = [...lettersInHand];
+
+  // loop through letters in input
+  // check if all character is in the updatedLetters
+  for (let letter of input) {
+    // letter = letter.toUpperCase();
+    for (const char in updatedLettersInHand) {
+      if (letter === char) {
+        //remove letter from updatedLetters
+        let indexOfLetter = updatedLettersInHand.indexOf(letter);
+        updatedLettersInHand.splice[indexOfLetter]
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  console.log(updatedLettersInHand);
+  return true;
 };
 
 export const scoreWord = (word) => {
